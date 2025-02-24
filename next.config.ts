ab7e4ceef5c8ebcdd944a121d4adee8fb4be4c79
Tosapon Chaiwong <tontosapon@gmail.com>
2025-02-25 00:44:42 +0700

    Configure the Github Actions

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath: "/7-solutions-assignment-1",
+  output: "export",  // enables static exports
+  reactStrictMode: true,
 };
 
 export default nextConfig;
