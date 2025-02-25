d22607d7d6191c9f60d4495a96ae144c3ebf2151
Tosapon Chaiwong <tontosapon@gmail.com>
2025-02-26 00:07:55 +0700

    Apply to Home page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: 'Next.js on GitHub Pages',
-  description: 'Deploy your static Next.js site to GitHub Pages.',
+  title: 'Auto Delete Todo List',
+  description: '7 Solutions Frontend Assignemnt - Auto Delete Todo List',
 };
 
 export default function RootLayout({
